613f49e2577d39ae51e7ca2cbae70ba9fac4a461
abderrahmane el-morhit <elmorit.abderrahmane.dev@gmil.com>
2021-04-13 11:20:25 +0000

    b

---
+++ 3-syntax.php
@@ -0,0 +1,11 @@
+<html>
+   <body>
+      
+      <?php
+         $capital = 67;
+         print("Variable capital is $capital<br>");
+         print("Variable capital is $capital<br>");
+      ?>
+      
+   </body>
+</html>
